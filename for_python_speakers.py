4591de0c1d827618ae4d17dfa7a51ca984a2ca69
Isus Ramzy <128253026+IsusRamzy@users.noreply.github.com>
2024-07-11 01:47:11 +0300

    Create for_python_speakers.py

---
+++ for_python_speakers.py
@@ -0,0 +1,17 @@
+class Person():
+  def __init__(self, name: str, knowledge: list[str], languages: list[str]):
+    self.name = name
+    self.knowledge = knowledge
+    self.languages = languages
+
+Isus = Person(name="Isus Ramzy Beshara",
+  knowledge=["Unity & C# (Learning...)", "HTML & CSS", "CustomTkinter", "Ursina", "Flask", "MongoDB"],
+  languages=["English (Intermediate)", "Arabic (Native)"]
+)
+print(f"Hello! I'm {Isus.name}")
+print("For programming, I know:")
+for item in Isus.knowledge:
+  print(item)
+print("When speaking, I know:")
+for item in Isus.languages:
+  print(item)
