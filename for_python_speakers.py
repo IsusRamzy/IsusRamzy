5558555b69eded898d4054972506b77cd0499fa2
Isus Ramzy <128253026+IsusRamzy@users.noreply.github.com>
2024-07-11 02:14:44 +0300

    Update for_python_speakers.py

--- for_python_speakers.py
+++ for_python_speakers.py
@@ -5,7 +5,7 @@ class Person():
     self.languages = languages
 
 Isus = Person(name="Isus Ramzy Beshara",
-  knowledge=["Unity & C# (Learning..)", "HTML & CSS", "CustomTkinter app framework", "Ursina game engine", "Flask micro web framework", "MongoDB database"],
+  knowledge=["Unity & C# (Learning..)", "HTML & CSS", "CustomTkinter app framework", "Ursina game engine", "Flask micro web framework", "MongoDB database", "Arduino micro-controller", "GPIO Zero library for Raspberry Pi Hardware control"],
   languages=["English (Advanced)", "Arabic (Native)"]
 )
 print(f"Hello! I'm {Isus.name}")
